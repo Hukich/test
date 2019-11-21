07666e594470b0a227176a593c089c78d8fb6566
Ruslan <hukruslan@hotmail.com>
2019-11-21 11:55:27 +0200

    add chages

--- test/script.js
+++ test/script.js
@@ -1,13 +1,11 @@
 let money = +prompt('Ваш бюджет на месяц?',0);
 let time = prompt('Введите дату в формате YYYY-MM-DD','2019-11-20');
-let section = prompt('Введите обязательную статью расходов в этом месяце');
-let howMuch = prompt('Во сколько обойдется?');
+
 
 let appData = {
     budget : money,
     timeData : time,
     expenses : {
-        section : howMuch,
     },
     optionalExpenses : {
 
@@ -18,4 +16,13 @@ let appData = {
     savings : false,
 
 };
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4; 
+
 alert(appData.budget/30);
